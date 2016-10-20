127f2a476ec3e4388c6bcc918ff0dd8f5923162a
ZyanZou <873863981@qq.com>
2016-10-20 22:00:56 +0800

    create to make springSessionRepositoryFilter work

---
+++ src/main/java/zyan/configure/HttpSessionConfig.java
@@ -0,0 +1,10 @@
+package zyan.configure;
+
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+/**
+ * Created by zouye on 2016/10/20.
+ */
+@EnableRedisHttpSession
+public class HttpSessionConfig {
+}
