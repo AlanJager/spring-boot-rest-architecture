9f692bd0b57aca40e21ea331e070e46b201375ed
AlanJager <873863981@qq.com>
2016-10-27 18:24:01 +0800

    bug fixed

--- src/main/java/alanjager/common/session/HttpSessionConfig.java
+++ src/main/java/alanjager/common/session/HttpSessionConfig.java
@@ -7,11 +7,4 @@ import org.springframework.session.data.redis.config.annotation.web.http.EnableR
  */
 @EnableRedisHttpSession
 public class HttpSessionConfig {
-//    private StringRedisTemplate stringRedisTemplate;
-//
-//    @Autowired
-//    public HttpSessionConfig(StringRedisTemplate stringRedisTemplate)
-//    {
-//        this.stringRedisTemplate = stringRedisTemplate;
-//    }
 }
