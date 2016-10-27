1d742fba865f208293001fd9f109544964f25259
AlanJager <873863981@qq.com>
2016-10-27 18:23:49 +0800

    redis bean

---
+++ src/main/java/alanjager/common/session/HttpSessionConfig.java
@@ -0,0 +1,17 @@
+package alanjager.common.session;
+
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+
+/**
+ * Created by AlanJager on 2016/10/20.
+ */
+@EnableRedisHttpSession
+public class HttpSessionConfig {
+//    private StringRedisTemplate stringRedisTemplate;
+//
+//    @Autowired
+//    public HttpSessionConfig(StringRedisTemplate stringRedisTemplate)
+//    {
+//        this.stringRedisTemplate = stringRedisTemplate;
+//    }
+}
