e063432aefc6fa011ee1c98bed0dc462e34d6da5
AlanJager <873863981@qq.com>
2016-10-27 18:35:16 +0800

    share session test

---
+++ src/main/java/alanjager/controller/TestController.java
@@ -0,0 +1,34 @@
+package alanjager.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by AlanJager on 2016/10/27.
+ */
+@RestController
+@RequestMapping(value = "test/v1")
+public class TestController {
+    @RequestMapping(value = "/create", method = RequestMethod.GET)
+    public Map<String, Object> firstRes (HttpServletRequest request)
+    {
+        Map<String, Object> map = new HashMap<>();
+        request.getSession().setAttribute("request Url", request.getRequestURL());
+        map.put("request Url", request.getRequestURL());
+        return map;
+    }
+
+    @RequestMapping(value = "/sessions", method = RequestMethod.GET)
+    public Object sessions (HttpServletRequest request)
+    {
+        Map<String, Object> map = new HashMap<>();
+        map.put("sessionId", request.getSession().getId());
+        map.put("message", request.getSession().getAttribute("map"));
+        return map;
+    }
+}
