b6ab1878b941fc4a711051a96c9e809936ef7d46
AlanJager <873863981@qq.com>
2016-10-28 12:26:19 +0800

    add simpile @Filter

---
+++ src/main/java/alanjager/filter/SessionFilter.java
@@ -0,0 +1,29 @@
+// Created by AlanJager on 2016/10/28.
+package alanjager.filter;
+
+import org.apache.log4j.Logger;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import java.io.IOException;
+
+@WebFilter(filterName = "SessionFilter", urlPatterns = "/*")
+public class SessionFilter implements Filter {
+    private Logger logger = Logger.getLogger(getClass());
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        logger.info("Session filter init");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        filterChain.doFilter(servletRequest, servletResponse);
+        logger.info("Session filter worked");
+    }
+
+    @Override
+    public void destroy() {
+        logger.info("Session filter destroyed");
+    }
+}
