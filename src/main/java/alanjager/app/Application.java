7374e0fadacd06e08841712830851b0239bdb9b9
AlanJager <873863981@qq.com>
2016-10-28 12:24:13 +0800

    add servlet scan

--- src/main/java/alanjager/app/Application.java
+++ src/main/java/alanjager/app/Application.java
@@ -7,12 +7,14 @@ package alanjager.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.orm.jpa.EntityScan;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @ComponentScan(basePackages = "alanjager")
+@ServletComponentScan(basePackages = "alanjager.filter")
 @EntityScan(basePackages = "alanjager.pojo")
 @EnableJpaRepositories(basePackages = "alanjager.repository")
 @EnableAutoConfiguration
