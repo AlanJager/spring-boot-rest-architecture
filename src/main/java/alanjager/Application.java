bfda651a3b9309a87ad9e09690a3c77ee7769e3b
AlanJager <873863981@qq.com>
2016-10-31 15:31:57 +0800

    cancel scan annotation

--- src/main/java/alanjager/Application.java
+++ src/main/java/alanjager/Application.java
@@ -6,18 +6,14 @@ package alanjager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.orm.jpa.EntityScan;
-import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @ComponentScan(basePackages = "alanjager")
-@ServletComponentScan(basePackages = "alanjager.common.filter")
-@EntityScan(basePackages = "alanjager.pojo")
-@EnableJpaRepositories(basePackages = "alanjager.repository")
 @EnableAutoConfiguration
+@SpringBootApplication
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
