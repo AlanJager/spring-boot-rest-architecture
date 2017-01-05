86bac0fee8b98a1540fa354016eda074712ef06c
AlanJager <873863981@qq.com>
2017-01-05 21:36:48 +0800

    update

--- src/main/java/alanjager/service/UserService.java
+++ src/main/java/alanjager/service/UserService.java
@@ -1,9 +1,9 @@
 package alanjager.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import alanjager.domain.User;
 import alanjager.domain.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by AlanJager on 2016/10/20.
